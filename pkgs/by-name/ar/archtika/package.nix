0cfbbd8c51483180b3d41952cfcd6c52989d2e65
thiloho <123883702+thiloho@users.noreply.github.com>
2024-12-14 16:34:33 +0100

    archtika: init at 1.0.1
    archtika: fix allow-import-from-derivation error for npm
    
    archtika: fix package formatting
    
    archtika: refactor package

---
+++ pkgs/by-name/ar/archtika/package.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  stdenv,
+  buildNpmPackage,
+  importNpmLock,
+  symlinkJoin,
+  fetchFromGitHub,
+}:
+
+let
+  version = "1.0.1";
+
+  src = fetchFromGitHub {
+    owner = "archtika";
+    repo = "archtika";
+    rev = "v${version}";
+    hash = "sha256-+zZ2v2kYpJ12bJYURt4Ax5Mt3zgr+WQjnxLbAx0DKY0=";
+  };
+
+  web = buildNpmPackage {
+    name = "web-app";
+    src = "${src}/web-app";
+    npmDepsHash = "sha256-RTyo7K/Hr1hBGtcBKynrziUInl91JqZl84NkJg16ufA=";
+    npmFlags = [ "--legacy-peer-deps" ];
+    installPhase = ''
+      mkdir -p $out/web-app
+      cp package.json $out/web-app
+      cp -r node_modules $out/web-app
+      cp -r build/* $out/web-app
+      cp -r template-styles $out/web-app
+    '';
+  };
+
+  api = stdenv.mkDerivation {
+    name = "api";
+    src = "${src}/rest-api";
+    installPhase = ''
+      mkdir -p $out/rest-api/db/migrations
+      cp -r db/migrations/* $out/rest-api/db/migrations
+    '';
+  };
+in
+symlinkJoin {
+  pname = "archtika";
+  version = version;
+
+  paths = [
+    web
+    api
+  ];
+
+  meta = {
+    description = "A modern, performant and lightweight CMS";
+    homepage = "https://archtika.com";
+    license = lib.licenses.gpl3;
+    maintainers = [ lib.maintainers.thiloho ];
+    platforms = lib.platforms.unix;
+  };
+}
